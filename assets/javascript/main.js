01a3dc74334ef2d58b4f0b77ab5c45a716e53078
iamparmarvk <parmarvishal1217@gmail.com>
2023-07-06 21:33:29 +0100

    Theme footer | Done

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,20 @@
+$(document).ready(function(){
+    var current_year = new Date().getFullYear()
+    document.getElementById('copyright_year').innerHTML = current_year
+})
+
 function navbarToggle(x) {
     x.classList.toggle("change");
-  }
+}
+document.getElementById("goToTop").addEventListener("click", function (event) {
+    event.preventDefault();
+    scrollToTop();
+});
+
+function scrollToTop() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > 0) {
+        window.requestAnimationFrame(scrollToTop);
+        window.scrollTo(0, scrollTop - scrollTop / 8);
+    }
+}
